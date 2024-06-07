3e5d41213ee4dd4bbbd251ffc479006e8b68448a
Zack Botkin <zackbotkin@gmail.com>
2024-06-07 15:32:22 -0500

    first commit

---
+++ query_runner/src/runner.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+class SqlLiteQueryRunner(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.database_file_name = "%s\\%s.db" % (
+            self.config.get("database_directory"),
+            self.config.get("database_name")
+        )
+
+    def run_sql(self, sql_str):
+        conn = sqlite3.connect(self.database_file_name)
+        conn.execute(sql_str)
+        conn.commit()
+
+    def fetch_sql(self, sql_str):
+        conn = sqlite3.connect(self.database_file_name)
+        query = conn.execute(sql_str)
+        results = query.fetchall()
+        return results
+
+    ## raise 
+    def initialize(self):
+        raise "Not Implemented"
